6a4cc0a09c72c515a717258915f282af3c1e6bd1
Harsh Pathak <hp238413@gmail.com>
2026-07-02 12:53:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (76.43%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.cpp
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
+
         int i = 0;
         int j = 0;
-        if(name[i]!=typed[j]){
-            return false;
-        }
-        
-        while(i<name.size() || j<typed.size()){
-            if(name[i]==typed[j]){
+
+        while (i < name.size() && j < typed.size()) {
+
+            if (name[i] == typed[j]) {
                 i++;
                 j++;
             }
-            else if(typed[j]==typed[j-1]){
+            else if (j > 0 && typed[j] == typed[j - 1]) {
                 j++;
             }
-            else{
+            else {
                 return false;
             }
         }
+
+        // agar j pehle khatam ho gya to false hoga 
+        if (i != name.size())
+            return false;
+
+        // agar i pehle khatam ho gya j se to j ke last sare letters same hone chahiye
+        while (j < typed.size()) {
+            if (typed[j] != typed[j - 1])
+                return false;
+            j++;
+        }
+
         return true;
     }
 };
