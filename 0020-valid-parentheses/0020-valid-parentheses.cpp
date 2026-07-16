633a7e2f0aeaa2b548ab7feb73ea2af4d19d2151
Harsh Pathak <hp238413@gmail.com>
2026-07-16 14:02:47 +0530

    Time: 2 ms (14.93%), Space: 8.9 MB (38.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,37 +3,28 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i = 0;i<s.size();i++){
-            if(st.empty()){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
                 st.push(s[i]);
             }
-
             else{
-                if(st.top()=='('){
-                    st.push(s[i]);
-                    if(st.top()==')'){
-                        st.pop();
-                        st.pop();
-                    }
+                if(st.empty()){
+                    return false;
                 }
-                else if(st.top()=='['){
-                    st.push(s[i]);
-                    if(st.top()==']'){
-                        st.pop();
-                        st.pop();
-                    }
+                
+                else if(st.top()=='(' && s[i]==')'){
+                     st.pop();
                 }
-                else if(st.top()=='{'){
-                    st.push(s[i]);
-                    if(st.top()=='}'){
-                        st.pop();
-                        st.pop();
-                    }
+                else if(st.top()=='[' && s[i]==']'){
+                     st.pop();
                 }
+                else if(st.top()=='{' && s[i]=='}'){
+                     st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
         }
-        }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        return (st.empty());    
     }
 };
