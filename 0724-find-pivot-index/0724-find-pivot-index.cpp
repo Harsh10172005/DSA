e3b3815397c4c0cffb614dea55ee8b1b10ccd8f9
Harsh Pathak <hp238413@gmail.com>
2026-08-10 21:29:40 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.77%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,14 +4,12 @@ public:
         int total_sum = accumulate(nums.begin(), nums.end(), 0);
         int left_sum = 0;
         int right_sum = 0;
-        int sum = 0;
         for(int i = 0;i<nums.size();i++){
-            sum+=nums[i];
-            left_sum = sum-nums[i];
-            right_sum = total_sum-sum;
+            right_sum = total_sum - left_sum - nums[i];
             if(left_sum==right_sum){
                 return i;
             }
+            left_sum +=nums[i];
         }
         return -1;
         
