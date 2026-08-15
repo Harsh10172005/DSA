dc420a1b566fa00e09fd2a01f1f64f170e6fda4a
Harsh Pathak <hp238413@gmail.com>
2026-08-15 11:44:37 +0530

    Time: 0 ms (100%), Space: 171.5 MB (4.61%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -6,15 +6,12 @@ public:
         for(int i = 0;i<nums.size();i++){
             totalXor^=nums[i];
             maxi = max(maxi,nums[i]);
-
         }
         if(totalXor!=0){
             return nums.size();
         }
-        else if(totalXor==0){
-            if(maxi>0){
+        else if(totalXor==0 && maxi>0){
                 return nums.size()-1;
-            }
         }
         return 0;
 
