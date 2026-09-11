9fa831631051a23dacb53b3f09f95e885a3d0df5
Harsh Pathak <hp238413@gmail.com>
2026-09-11 16:37:18 +0530

    Time: 0 ms (100%), Space: 15 MB (59.4%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,29 +11,43 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
-        int count = 0;
-        while(temp!=nullptr){
-            count++;
-            temp = temp->next;
+        // ListNode* temp = head;
+        // int count = 0;
+        // while(temp!=nullptr){
+        //     count++;
+        //     temp = temp->next;
+        // }
+        // count=count-n;
+        // if(count==0){
+        //     return head->next;
+        // }
+        // int count1 =0;
+        // ListNode* temp1 = head;
+        // ListNode* prev = nullptr;
+        // while(temp1!=nullptr){
+        //     if(count1==count){
+        //         prev->next = prev->next->next;
+        //     }
+        //     else{
+        //         prev = temp1;
+        //         temp1 = temp1->next;
+        //     }
+        //     count1++;
+        // }
+        // return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* fast = &dummy;
+        ListNode* slow = &dummy;
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
         }
-        count=count-n;
-        if(count==0){
-            return head->next;
+        while(fast->next != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
         }
-        int count1 =0;
-        ListNode* temp1 = head;
-        ListNode* prev = nullptr;
-        while(temp1!=nullptr){
-            if(count1==count){
-                prev->next = prev->next->next;
-            }
-            else{
-                prev = temp1;
-                temp1 = temp1->next;
-            }
-            count1++;
-        }
-        return head;        
+        slow->next = slow->next->next;        
+        return dummy.next;
     }
 };
